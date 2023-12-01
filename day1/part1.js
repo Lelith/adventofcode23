77c848630176b035a45a8f41eb9994b40d4749b8
Kathrin <kathrin.holzmann@contentful.com>
2023-12-01 13:45:27 +0100

    part 1 solution

--- day1/part1.js
+++ day1/part1.js
@@ -1,8 +1,26 @@
 const utils = require("../utils");
 
+function findNumbers(sequence) {
+  const regExp = /\d/g ;
+  return sequence.map(element => {
+    return element.match(regExp);
+  });
+}
+
+function generateSum(identifiedNumbers){
+  const sumNumbers = identifiedNumbers.map((sequenceNumbers)=>{
+    const length = sequenceNumbers.length;
+    return parseInt(sequenceNumbers[0]+""+sequenceNumbers[length-1],10);
+  })
+  console.log(utils.calcArraySum(sumNumbers));
+}
+
 try {
-  // let data = utils.readInput('./example.txt');
-  // let data = utils.readInput('./input.txt');
+  //let data = utils.readInput('./example.txt');
+  let data = utils.readInput('./input.txt');
+  data = utils.modDataNewlineStr(data);
+  const sequenceNumbers = findNumbers(data);
+  generateSum(sequenceNumbers);
 } catch (e) {
   console.log("Error", e.stack);
 }
